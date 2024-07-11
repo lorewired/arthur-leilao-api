5a209d23c779161b7fe143be57a2982f8abd8807
= <lorecode12@gmail.com>
2024-07-11 07:21:22 +0000

    to railway

--- internal/domain/router.go
+++ internal/domain/router.go
@@ -47,7 +47,7 @@ func (r *Router) Init(db *sql.DB) {
 func (r *Router) Start() {
 	fmt.Println("server is already running at PORT: " + r.port)
 
-	if err := http.ListenAndServe(":"+r.port, handlers.CORS(
+	if err := http.ListenAndServe("0.0.0.0:"+r.port, handlers.CORS(
 		handlers.AllowedOrigins([]string{"http://localhost:5173"}),
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
 		handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
